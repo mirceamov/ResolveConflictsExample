5db023c7a803924d6acd15d98612f77a88cd9f58
Mircea Movila <mirceamov@gmail.com>
2017-12-12 21:25:50 +0200

    Added the first and second texts

---
+++ ResolveConflictsExample/ResolveConflictsExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolveConflictsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("first line of text");
+            Console.WriteLine("second line of text");
+            Console.Read();
+        }
+    }
+}
